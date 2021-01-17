b7e6b9893bb76d457d7402c1fda17655c9d9a511
KevinXu17 <xuzhaozhishu@gmail.com>
2021-01-17 11:28:29 -0800

    New style camp blog

--- app.js
+++ app.js
@@ -1,59 +1,167 @@
-    var express       = require("express")
-    var app           = express();
-    var bodyParser    = require("body-parser")
-    var mongoose      = require("mongoose");
-    var Campground    = require("./models/campground");
-    var Comment       = require("./models/comment");
-    var User          = require("./models/user");
-    var seedDB        = require("./seeds")
-    var passport      = require("passport")
-    var LocalStrategy = require("passport-local")
-    var flash         = require("connect-flash")
-    
-    var commentRoutes   = require("./routes/comments")
-    var campgroudRoutes = require("./routes/campgrounds")
-    var indexRoutes     = require("./routes/index")
-    
-    var methodOverride = require("method-override")
-    
-    app.use(bodyParser.urlencoded({extended: true}));
-    app.set("view engine", "ejs");
-    
-    var url = process.env.DATABASEURL || "mongodb://localhost/yelp_camp_v3"
-    mongoose.connect(url);
-   
-    
-    app.use(express.static(__dirname + "/public"))
-    app.use(methodOverride("_method"))
-    app.use(flash())
-   // seedDB();
-    
-    // PASSPORT CONFIGURATION
-    app.use(require("express-session")({
-        secret: "What do you want",
-        resave: false,
-        saveUninitialized: false
-    }))
-    app.use(passport.initialize());
-    app.use(passport.session());
-    passport.use(new LocalStrategy(User.authenticate()));
-    passport.serializeUser(User.serializeUser());
-    passport.deserializeUser(User.deserializeUser());
-        
-   // this must after all of above        
-    app.use(function(req, res, next){   // add currentuser to all rout
-        res.locals.currentUser = req.user;
-        res.locals.error = req.flash("error")
-        res.locals.success = req.flash("success") 
-        next();
-    })
-   
-  app.use(indexRoutes);
-  app.use("/campgrounds", campgroudRoutes); // make all campground routes with predix /campgrounds that
-                                            // campgrounds routes dont need to have it all 
-  app.use("/campgrounds/:id/comments", commentRoutes)
-    
+if (process.env.NODE_ENV !== "production") {
+    require('dotenv').config();
+}
 
-    app.listen(process.env.PORT, process.env.IP, function() {
-        console.log("Server is Open!");
-    });
+const express = require('express');
+const path = require('path');
+const mongoose = require('mongoose');
+const ejsMate = require('ejs-mate');
+const session = require('express-session');
+const flash = require('connect-flash');
+const ExpressError = require('./utils/ExpressError');
+const methodOverride = require('method-override');
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
+const User = require('./models/user');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const userRoutes = require('./routes/users');
+const campgroundRoutes = require('./routes/campgrounds');
+const reviewRoutes = require('./routes/reviews');
+
+const MongoDBStore = require("connect-mongo")(session);
+
+const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp';
+
+mongoose.connect(dbUrl, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false
+});
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+    console.log("Database connected");
+});
+
+const app = express();
+
+app.engine('ejs', ejsMate)
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'))
+
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
+app.use(express.static(path.join(__dirname, 'public')))
+app.use(mongoSanitize({
+    replaceWith: '_'
+}))
+const secret = process.env.SECRET || 'thisshouldbeabettersecret!';
+
+const store = new MongoDBStore({
+    url: dbUrl,
+    secret,
+    touchAfter: 24 * 60 * 60
+});
+
+store.on("error", function (e) {
+    console.log("SESSION STORE ERROR", e)
+})
+
+const sessionConfig = {
+    store,
+    name: 'session',
+    secret,
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        httpOnly: true,
+        // secure: true,
+        expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+        maxAge: 1000 * 60 * 60 * 24 * 7
+    }
+}
+
+app.use(session(sessionConfig));
+app.use(flash());
+app.use(helmet());
+
+
+const scriptSrcUrls = [
+    "https://stackpath.bootstrapcdn.com/",
+    "https://api.tiles.mapbox.com/",
+    "https://api.mapbox.com/",
+    "https://kit.fontawesome.com/",
+    "https://cdnjs.cloudflare.com/",
+    "https://cdn.jsdelivr.net",
+];
+const styleSrcUrls = [
+    "https://kit-free.fontawesome.com/",
+    "https://stackpath.bootstrapcdn.com/",
+    "https://api.mapbox.com/",
+    "https://api.tiles.mapbox.com/",
+    "https://fonts.googleapis.com/",
+    "https://use.fontawesome.com/",
+];
+const connectSrcUrls = [
+    "https://api.mapbox.com/",
+    "https://a.tiles.mapbox.com/",
+    "https://b.tiles.mapbox.com/",
+    "https://events.mapbox.com/",
+];
+const fontSrcUrls = [];
+app.use(
+    helmet.contentSecurityPolicy({
+        directives: {
+            defaultSrc: [],
+            connectSrc: ["'self'", ...connectSrcUrls],
+            scriptSrc: ["'unsafe-inline'", "'self'", ...scriptSrcUrls],
+            styleSrc: ["'self'", "'unsafe-inline'", ...styleSrcUrls],
+            workerSrc: ["'self'", "blob:"],
+            objectSrc: [],
+            imgSrc: [
+                "'self'",
+                "blob:",
+                "data:",
+                "https://res.cloudinary.com/douqbebwk/", //SHOULD MATCH YOUR CLOUDINARY ACCOUNT! 
+                "https://images.unsplash.com/",
+            ],
+            fontSrc: ["'self'", ...fontSrcUrls],
+        },
+    })
+);
+
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.use((req, res, next) => {
+    res.locals.currentUser = req.user;
+    res.locals.success = req.flash('success');
+    res.locals.error = req.flash('error');
+    next();
+})
+
+
+app.use('/', userRoutes);
+app.use('/campgrounds', campgroundRoutes)
+app.use('/campgrounds/:id/reviews', reviewRoutes)
+
+
+app.get('/', (req, res) => {
+    res.render('home')
+});
+
+
+app.all('*', (req, res, next) => {
+    next(new ExpressError('Page Not Found', 404))
+})
+
+app.use((err, req, res, next) => {
+    const { statusCode = 500 } = err;
+    if (!err.message) err.message = 'Oh No, Something Went Wrong!'
+    res.status(statusCode).render('error', { err })
+})
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Serving on port ${port}`)
+})
+
+
--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -1,114 +1,28 @@
- var express = require("express")
- var router = express.Router();
- var Campground = require("../models/campground")
- var Comment = require("../models/comment")
- var middlewareObj = require("../middleware")
- // INDEX show all campgrounds
-    router.get("/", function(req, res) {
-           // get all campgrounds from db
-           Campground.find({}, function(err, Allcampgrounds) {
-               if (err) {
-                   req.flash("error", "Not campgrounds can found")
-                   console.log(err);
-               }else {
-                   res.render("campgrounds/index", {
-                       campgrounds: Allcampgrounds
-                   });
-               }
-           })
-    });
-    // NEW: show the form data
-    router.get("/new", middlewareObj.isLoggedIn, function(req, res) {
-        res.render("campgrounds/new");
-    })
-    
-    // CREATE: where allow u to change campgournds make a new campgrounds
-    router.post("/", middlewareObj.isLoggedIn, function(req, res) {
-        // get data from form and add to campgronds array
-        //redirect back to campground page
-        //res.send("!!!!!");
-        var name = req.body.name;
-        var image = req.body.image;
-        var price = req.body.price;
-        var description = req.body.description;
-        var author = {
-            id: req.user._id,
-            username: req.user.username
-        }
-        var newCampGround = {name: name,
-                             price: price,
-                             image: image,
-                             description: description,
-                             author: author
-        };
-        Campground.create(newCampGround, function(err, newCamp) {
-            if (err) {
-                req.flash("error", "Can not make a new campground, please try again.")
-               console.log(err);
-            } else {
-                console.log("make a new campgournd");
-                console.log(newCamp)
-                        res.redirect("/campgrounds"); // here is the web not file
-            }
-        });        
-    });
-    
-    // SHOW
-    router.get("/:id", function(req, res) {
-        // find campgroud with id
-        Campground.findById(req.params.id).populate("comments").exec(function(err, foundCampground) {
-           if (err) {
-               req.flash("error", "Campground can not found.")
-               console.log(err);
-           } else {
-               console.log(foundCampground)
-               res.render("campgrounds/show", {campground: foundCampground});
-           }
-        });
-        // render the id campgroud
-       // res.send("show");
-    })
-    
-    // EDIT Campground
-    router.get("/:id/edit", middlewareObj.checkCampgroundOwnership, function(req, res) {
-                Campground.findById(req.params.id, function(err, foundCampground) {
-                    res.render("campgrounds/edit", {campground: foundCampground})
-                })
-    })
-    // UPDATE Campground
-    router.put("/:id", middlewareObj.checkCampgroundOwnership, function(req, res) {
-        // find and update campground
-        Campground.findByIdAndUpdate(req.params.id, req.body.campground, 
-        function(err, updateCampground){
-            if (err) {
-                req.flash("error", "Can not update campground, please try again.")
-                console.log(err)
-                res.redirect("/campgrounds")
-            }else {
-                req.flash("success", "Campground updated.")
-                res.redirect("/campgrounds/" + req.params.id);
-            }
-        })
-        // redirect show page
-    })
-    
-    // DESTORY
-    router.delete("/:id", middlewareObj.checkCampgroundOwnership, function(req, res) {
-        //findId and remove
-        Campground.findByIdAndRemove(req.params.id, function(err) {
-            if (err) {
-                req.flash("error", "Can not remove campground, please try again")
-                console.log(err);
-                res.redirect("/campgrounds")
-            } else {
-                req.flash("success", "Campground deleted.")
-                 res.redirect("/campgrounds")
-            }
-        })
-    })
-    
+const express = require('express');
+const router = express.Router();
+const campgrounds = require('../controllers/campgrounds');
+const catchAsync = require('../utils/catchAsync');
+const { isLoggedIn, isAuthor, validateCampground } = require('../middleware');
+const multer = require('multer');
+const { storage } = require('../cloudinary');
+const upload = multer({ storage });
 
-    
+const Campground = require('../models/campground');
 
-    module.exports = router;
-    
+router.route('/')
+    .get(catchAsync(campgrounds.index))
+    .post(isLoggedIn, upload.array('image'), validateCampground, catchAsync(campgrounds.createCampground))
+
+
+router.get('/new', isLoggedIn, campgrounds.renderNewForm)
+
+router.route('/:id')
+    .get(catchAsync(campgrounds.showCampground))
+    .put(isLoggedIn, isAuthor, upload.array('image'), validateCampground, catchAsync(campgrounds.updateCampground))
+    .delete(isLoggedIn, isAuthor, catchAsync(campgrounds.deleteCampground));
+
+router.get('/:id/edit', isLoggedIn, isAuthor, catchAsync(campgrounds.renderEditForm))
+
+
+
+module.exports = router;
