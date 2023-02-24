6977edb2f4374af5bea1e857d9c9c860504bf0ce
KevinXu17 <xuzhaozhishu@gmail.com>
2023-02-23 22:51:51 -0800

    reset env

--- app.js
+++ app.js
@@ -21,8 +21,7 @@ const reviewRoutes = require('./routes/reviews');
 
 const MongoDBStore = require("connect-mongo")(session);
 
-
-process.env.DB_URL = 'mongodb+srv://yelpcamp:wen6656847@cluster0.nvvni.mongodb.net/?retryWrites=true&w=majority'
+// TODO when deploy
 const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp';
 
 mongoose.connect(dbUrl, {
